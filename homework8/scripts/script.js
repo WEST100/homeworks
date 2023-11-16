d0f26ca7f1da0b420edcaf93056e774d16e51631
WEST100 <semeniv.denis@gmail.com>
2023-11-16 20:18:06 +0100

    commitHomeWork8

--- homework8/scripts/script.js
+++ homework8/scripts/script.js
@@ -22,38 +22,40 @@
 
 // 3. Написать цикл, который проходится по массиву с объектами - у объектов свойства first_name, last_name и  age (данные взять произвольные) - и создает карточки только для совершеннолетних пользователей. Карточка должна содержать информацию об имени, фамилии и возрасте пользователя. Добавить все карточки в div с классом users_container.
 
-// const data = [
-//     {
-//         first_name: "Alex",
-//         last_name: "West",
-//         age: '15',
-//     },
-//     {
-//         first_name: "John",
-//         last_name: "East",
-//         age: '32',
-//     },
-//     {
-//          first_name: "Eric",
-//         last_name: "South",
-//         age: '17',
-//     },
-//     {
-//         first_name: "Anna",
-//         last_name: "North",
-//         age: '25',
-//     }
-// ]
+const data = [
+    {
+        first_name: "Alex",
+        last_name: "West",
+        age: '15',
+    },
+    {
+        first_name: "John",
+        last_name: "East",
+        age: '32',
+    },
+    {
+         first_name: "Eric",
+        last_name: "South",
+        age: '17',
+    },
+    {
+        first_name: "Anna",
+        last_name: "North",
+        age: '25',
+    }
+]
 
-// const dataName = document.querySelector(".users_container")
-// for (let i = 0; i <= data.length; i++) {
-//   if (data[i].age >= 18) {
-//   const pElem = document.createElement('p')
-//   pElem.innerText = data[i].first_name 
-//   pElem.innerText = data[i].last_name 
-//   pElem.innerText = data[i].age
+const dataName = document.querySelector(".users_container")
+for (let i = 0; i <= data.length; i++) {
+  if (data[i].age >= 18) {
+    const pElem = document.createElement('p')
+    const pElem2 = document.createElement('p')
+    const pElem3 = document.createElement('p')
+  pElem.innerText = data[i].first_name 
+  pElem2.innerText = data[i].last_name 
+  pElem3.innerText = data[i].age
 
-//   const br = document.createElement("br")
-//   dataName.append(pElem, br)
-// }
-// }
+  const br = document.createElement("br")
+  dataName.append(pElem, pElem2, pElem3, br)
+}
+}
